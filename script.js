ab60295a41c8711ce68c31fed91bb70de03ae786
orianaperdomo11 <orianaperdomo11@gmail.com>
2022-07-21 11:20:11 -0300

    todo ok

---
+++ script.js
@@ -0,0 +1,22 @@
+function agregarNotas(){ 
+    let nombre = prompt ("Ingrese el nombre completo del estudiante")
+    console.log(nombre)
+    let nota1 = parseInt(prompt ("Ingrese primera nota"))
+    let nota2 = parseInt(prompt ("Ingrese segunda nota"))
+    let nota3 = parseInt(prompt ("Ingrese tercer nota"))
+    
+    if (nota1 == ""){
+        alert ("Los espacios no pueden quedar vacios")
+    }
+    if (nota2 == ""){
+        alert ("Los espacios no pueden quedar vacios")
+    }
+    if (nota3 == ""){
+        alert ("Los espacios no pueden quedar vacios")
+    }
+    function operacion() {
+        let nota1=
+         
+
+    }
+}
